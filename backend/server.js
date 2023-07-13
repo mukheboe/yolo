4978bd9d4e6079c1bf21c17570a93041076b8c44
mukheboe <mukheboe@gmail.com>
2023-07-13 21:30:20 +0300

    added containers

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,32 @@ let db = mongoose.connection;
 db.once('open', ()=>{
     console.log('Database connected successfully')
 })
+// for containers
+const ronin 		= require( 'ronin-server' )
+const database  = require( 'ronin-database' )
+const mocks 		= require( 'ronin-mocks' )
+
+async function main() {
+
+    try {
+    await database.connect( process.env.CONNECTIONSTRING )
+    
+    const server = ronin.server({
+            port: process.env.SERVER_PORT
+        })
+
+        server.use( '/', mocks.server( server.Router()) )
+
+    const result = await server.start()
+        console.info( result )
+    
+    } catch( error ) {
+        console.error( error )
+    }
+}
+
+main()
+
 
 // Check for DB Errors
 db.on('error', (error)=>{
